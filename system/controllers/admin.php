6268cf3906ebdb49b809db5dc0b74644b711fc07
= <=>
2022-07-04 13:16:53 +0800

    add tempalte

--- system/controllers/admin.php
+++ system/controllers/admin.php
@@ -3,7 +3,13 @@
 class admin extends Controller{
     public function index()
     {
+        $this->view('layout/header');
+        $this->view('layout/navbar');
+        $this->view('layout/sidebar');
+
         $this->view('admin/index');
+
+        $this->view('layout/footer');
     }
     public function table()
     {
