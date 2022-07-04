163be26e9a892574ad8ab06e2e3caca8d590bbbf
SyaidiPutra <48019737+SyaidiPutra@users.noreply.github.com>
2022-07-04 22:14:41 +0800

    update siswa.php

--- system/controllers/admin/siswa.php
+++ system/controllers/admin/siswa.php
@@ -2,8 +2,17 @@
 
 class siswa extends Controller{
   
-  public function index ()
+  public function create ()
   {
-    
+    $this->view('layout/header');
+    $this->view('layout/navbar', [
+              'title' => 'Siswa',
+              'sunTitle' => 'Create'
+                ]);
+    $this->view('layout/sidebar');
+
+    $this->view('admin/siswa/create');
+
+    $this->view('layout/footer');
   }
 }
