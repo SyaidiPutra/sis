ca263c0023e41db49843b71669e129555e49fe07
SyaidiPutra <48019737+SyaidiPutra@users.noreply.github.com>
2022-07-06 14:02:52 +0800

    update Auth.php login

--- system/controllers/Auth.php
+++ system/controllers/Auth.php
@@ -1,8 +1,27 @@
 <?php
 
 class Auth {
+    public function __construct(){
+      $this->db = new Databases;
+      $this->input = new Request;
+    }
+    public function index()
+    {
+      
+    }
     public function login()
     {
-        
+       if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $input = $this->input->post();
+      $user = $this->db->table('user')->where('username',$input['username'])->first()->get();
+      if($user){
+        if(password_verify($user['password'],$input['password']){
+          
+          }
+        }
+      }
+      }else{
+        echo 'erorr 403 : access denied';
+      }
     }
 }
