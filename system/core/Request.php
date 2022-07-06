a896b46f925e9207cc5c85bc3af30aa2fd33ce79
SyaidiPutra <48019737+SyaidiPutra@users.noreply.github.com>
2022-07-06 13:12:31 +0800

    create fun get

--- system/core/Request.php
+++ system/core/Request.php
@@ -1,1 +1,13 @@
+<?php
 
+class Request {
+  public function post($key = null)
+  {
+    if($key == null){
+      $post = (isset($_POST))? $_POST : null;
+    }else{
+      $post = (isset($_POST[$key]))? $_POST[$key] : null;
+    }
+    return $post;
+  }
+}
