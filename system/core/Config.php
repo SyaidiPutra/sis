c198e58700c7b18b295f7b51afd1e2e4b87950f0
SyaidiPutra <syaidiputra29@gmai.com>
2022-07-09 17:21:34 +0800

    add config

---
+++ system/core/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+class Config {
+
+    public function __construct()
+    {
+        $this->data = json_decode(file_get_contents('../system/config.cof'),true);
+    }
+
+    public function config($key)
+    {
+        return $this->data[$key];
+    }
+    public function addConfig($config, $key, $val)
+    {
+
+    }
+
+}
